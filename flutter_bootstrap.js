130978b54b50fc7c8181ae3197e4cabc717670c1
binruba <binruba@users.noreply.github.com>
2024-07-29 00:30:56 +0000

    deploy: binruba/Source@2bf14bf31573647875cd391cd1f3da5d779a4348

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1228002489"
+    serviceWorkerVersion: "2614537823"
   }
 });
